29dbb71bf10a82bc77d45f7a2ad06cabb69fe887
gjw0 <1668447489@qq.com>
2020-04-22 10:22:21 +0800

    修改mycontroller

---
+++ testgit/src/main/java/com/kk/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.kk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/a")
+	@ResponseBody
+	public String a() {
+		System.out.println("bbb---");
+		return "a";
+	}
+	
+	
+}
