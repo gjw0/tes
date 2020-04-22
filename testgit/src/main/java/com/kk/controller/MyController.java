5ebc282fc925d6c675d414abe09242ceb5c9735b
gjw0 <1668447489@qq.com>
2020-04-22 21:55:59 +0800

    sdfasdfass

--- testgit/src/main/java/com/kk/controller/MyController.java
+++ testgit/src/main/java/com/kk/controller/MyController.java
@@ -12,6 +12,14 @@ public class MyController {
 	public String a() {
 		
 		System.out.println("aann---");
+		int m = 4;
+		while(m < 10) {
+			System.out.println("sdfasdf");
+			m++;
+		}
+		if(true) {
+			System.out.println("sfasdfasdfsdfsadff");
+		}
 		System.out.println("zznn---");
 		
 		return "a";
