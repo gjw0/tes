94b7bf4258d612e008457596cc9c37e09e074501
gjw0 <1668447489@qq.com>
2020-04-22 22:12:04 +0800

    sdfasdferertyrty

--- testgit/src/main/java/com/kk/controller/MyController.java
+++ testgit/src/main/java/com/kk/controller/MyController.java
@@ -12,6 +12,21 @@ public class MyController {
 	public String a() {
 		
 		System.out.println("aann---");
+		int i = 0;
+		while(i<10) {
+			System.out.println("sdfassdfas---");
+			i++;
+		}
+		String name = "dfasdfsa";
+		if (true) {
+			int ass = 4;
+			System.out.println("asdfaswersdfasdf");
+		}
+		
+		int s = 23;
+		String aaa = "sdfasdffasdfsa";
+		
+		
 		System.out.println("zznn---");
 		
 		return "a";
